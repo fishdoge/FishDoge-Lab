9f31f921dc46915782295f60f8b757f18ca14e7d
ru19wu06 <43335377+ru19wu06@users.noreply.github.com>
2022-11-02 17:04:01 +0800

    update button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,10 @@ const Navbar = ()=>{
   return(
     <nav className='w-full flex py-6 justify-between items-cender navbar'>
       <img src={LOGO2} alt="hoobank" className="w-[104px] h-[104px]"/>
+      {/*  #b7ff1a */}
+      
       <ul className="list-none font-bold sm:flex hidden hidden justify-end items-center flex-1">
+      <button className="bg-[#b7ff1a]  hover:bg-blue-700 text-[#000000] font-bold justify-end mr-[30px] py-2 px-4 rounded-full">中文</button>
         {navLinks.map((nav,index)=>(
           <li
             key ={nav.id}
