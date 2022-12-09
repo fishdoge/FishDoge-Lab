4c81e605d91b854b4753d0991bb5227cc40d162b
unknown <ru19wu06@gmail.com>
2022-12-09 16:34:33 +0800

    update contract

--- Consensus.sol
+++ Consensus.sol
@@ -4,31 +4,41 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract FishDogeLabConsensusConf is ERC1155,Ownable{
+    using Strings for uint256;
 
     uint256 private constant TotalSupply = 5000;
     uint256 private TotalMint = 0;
 
-    string private _name = "FishDoge Lab";
+    mapping(address => bool) public checkNFT;
+    mapping(address => bool) public checkNFTV2;
+
+    string private _name = "FishDoge Lab business card";
     string private _symbol = "FishDoge Lab";
 
+    string private baseURI;
+
     bool private mintStart = false;
-    
+
 
     constructor(string memory _baseURI)ERC1155(_baseURI){
-
+        baseURI = _baseURI;
     }
 
 
     fallback() external payable{
         require(mintStart,"not start yet");
-        
-        if(balanceOf(msg.sender,1) == 0){
-            mintFishDogeNamecard();
-        }else{
-            require(msg.value > 0.001 ether,"If you want a nother one please pay!");
 
+        if(checkNFT[msg.sender] == false){
+            mintFishDogeNamecard();
+            checkNFT[msg.sender] = true;
+        }else if(checkNFTV2[msg.sender] == false){
+            mintFishDogeNamecardV2();
+            checkNFTV2[msg.sender] = true;
+        }else{
+            revert("You've got it all");
         }
 
         payable(owner()).transfer(address(this).balance);
@@ -39,11 +49,15 @@ contract FishDogeLabConsensusConf is ERC1155,Ownable{
         _mint(msg.sender,1,1,"");
     }
 
+    function mintFishDogeNamecardV2() internal {
+        _mint(msg.sender,2,1,"");
+    }
+
     function setActive(bool state) external onlyOwner{
         mintStart = state;
     }
 
-    
+
     function name() public view returns (string memory) {
         return _name;
     }
@@ -56,9 +70,14 @@ contract FishDogeLabConsensusConf is ERC1155,Ownable{
         return TotalSupply;
     }
 
-   
+
     function balanceOf(address account)external view returns(uint256){
         return balanceOf(account,1);
+    }
+
+    function uri(uint256 typeId) public view override returns (string memory){
+
+        return string(abi.encodePacked(baseURI, typeId.toString(),".json"));
     }
 
 
